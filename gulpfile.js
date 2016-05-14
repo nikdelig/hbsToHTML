b158e72f4dc17889029646159cc414c24527328d
Nikos Deligeorgis <Nik@android-ea39552fbc2036e1.speedport_w_724v_typ_a_05011603_00_009>
2016-05-14 10:38:54 +0200

    Gulp file edited and html minifier added

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,57 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var hbsAll = require('gulp-compile-handlebars');
-var rename = require("gulp-rename");
-var less = require('gulp-less-sourcemap');
+var hbsAll = require('gulp-handlebars-all');
+var handlebars = require('handlebars');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var path = require('path');
 var autoprefixer = require('gulp-autoprefixer');
+var htmlmin = require('gulp-html-minifier');
+const imagemin = require('gulp-imagemin');
+var reporter = require('gulp-less-reporter');
 var watch = require('gulp-watch');
+
+
+handlebars.registerPartial('header', '{{header}}'),
+handlebars.registerPartial('details', '{{details}}'),
+handlebars.registerPartial('info', '{{info}}'),
+handlebars.registerPartial('prefooter', '{{prefooter}}'),
+handlebars.registerPartial('footer', '{{footer}}')
 
 
 gulp.task('default', function () {
 
-  options = {
-    batch : ['templates/partials'],
-  }
+    options = {
+        partialsDirectory : ['./templates/partials/**']
+    }
+
+    return gulp.src('templates/index.hbs')
+        .pipe(gulpHandlebars( options))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('../'));
+});
+
+gulp.task('hbsToHTML', function() {
+   gulp.src('templates/*.hbs')
+  .pipe(hbsAll('html', {
+    context: {foo: 'bar'},
  
-  return gulp.src('templates/index.handlebars')
-    .pipe(handlebars(options))
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest('templates'));
+    partials: ['templates/partials/**/*.hbs'],
+ 
+  }))
+  .pipe(rename('index.html'))
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest(''));
 });
 
 gulp.task('less', function () {
-  gulp.src('./less/*.less')
+  return gulp.src('./less/**/*.less')
     .pipe(less({
-        sourceMap: {
-            sourceMapRootpath: '../less' // Optional absolute or relative path to your LESS files
-        }
+      paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./css'));
 });
+
 
 gulp.task('prefix', function () {
   return gulp.src('css/main.css')
@@ -38,9 +62,21 @@ gulp.task('prefix', function () {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', ['less', 'prefix']);
+gulp.task('reporter', function() {
+  gulp.src('less/main.less').
+    pipe(less()).on('error', reporter);
+});
 
-gulp.task('watch', [ 'default', 'less'], function () {
+gulp.task('image', () =>
+  gulp.src('templates/partials/**/*.png')
+    .pipe(imagemin())
+    .pipe(gulp.dest('img'))
+);
+
+gulp.task('default', ['hbsToHTML', 'less', 'prefix', 'reporter', 'image']);
+
+gulp.task('watch', [ 'default', 'hbsToHTML', 'less', 'prefix'], function () {
   gulp.watch('templates/partials/**/*.hbs', ['default']);
+  gulp.watch('templates/**/*.hbs', ['hbsToHTML']);
   gulp.watch('less/**/*.less', ['less']);
 });
